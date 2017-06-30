70e8e50586a48bef6225ff4cb9a7a46e44244768
ck <1052650769@qq.com>
2017-06-30 17:28:32 +0800

    修改

--- m/index/upload_car.php
+++ m/index/upload_car.php
@@ -1,14 +1,5 @@
 <?php
-/*
- 本软件版权归作者所有,在投入使用之前注意获取许可
- 作者：北京市普艾斯科技有限公司
- 项目：simcms_锐车1.0
- 电话：010-58480317
- Q  Q: 228971357
- 网址：http://www.simcms.net
- simcms.net保留全部权力，受相关法律和国际公约保护，请勿非法修改、转载、散播，或用于其他赢利行为，并请勿删除版权声明。
-*/
-//if (!defined('APP_IN')) exit('Access Denied');
+if (!defined('APP_IN')) exit('Access Denied');
 
 include('json.php');
 
@@ -71,7 +62,7 @@ if (empty($_FILES) === false) {
 	$file_size = $_FILES['imgFile']['size'];
 	//检查文件名
 	if (!$file_name) {
-		alert("请选择文件。");
+		alert('111'.$_FILES['imgFile']['name']);
 	}
 	//检查目录
 	if (@is_dir($save_path) === false) {
