55ce19c8da31274dac7f94f4992e4bd5fa72a50b
ck <1052650769@qq.com>
2017-07-03 15:19:32 +0800

    修改

--- m/index/user.php
+++ m/index/user.php
@@ -512,7 +512,7 @@ elseif ($ac == 'addcar' || $ac == 'editcar') {
 // 添加图片
 elseif ($ac == 'addpicture') {
 	// 添加或修改
-	if (submitcheck('a')) {
+	if (submitcheck('a')) {echo 'abcdefg';
 		$arr_not_empty = array();
 		can_not_be_empty($arr_not_empty, $_POST);
 		$post = post('p_mainpic','p_backpic','p_foresightpic','p_leftforepic','p_rightforepic','p_backsightpic','p_leftbackpic','p_rightbackpic','p_leftpic','p_sidepic','p_wheelpic','p_rightpic','p_driverlicpic','p_drivinglicpic');
