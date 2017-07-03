573cd45d5ca5a0049097db7fb123245b8aafab25
ck <1052650769@qq.com>
2017-07-03 13:45:39 +0800

    修改

--- m/index/upload_car.php
+++ m/index/upload_car.php
@@ -25,4 +25,19 @@ $picname = trim($_POST['picname']);
 
 $output = "";
 $index = 0;	//$_FILES 以文件name为数组下标，不适用foreach($_FILES as $index=>$file)
-echo $picname;
+foreach($_FILES as $file){
+	$upload_filename = 'upload_file' . $index;//对应index.html FomData中的文件命名
+	$filename = $_FILES[$upload_filename]['name'];
+	//获得文件扩展名
+	$temp_arr = explode(".", $filename);
+	$file_ext = array_pop($temp_arr);
+	$file_ext = trim($file_ext);
+	$file_ext = strtolower($file_ext);
+	//新文件名
+	$rand_str = rand(10000, 99999);
+	$new_filename = date("YmdHis") . '_' . $rand_str . '.' . $file_ext;
+	$new_filename_small = date("YmdHis") . '_' . $rand_str . '_small.' . $file_ext;
+
+	$index++;
+}
+echo $output;
