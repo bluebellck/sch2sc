a66b05d4ed7ba2b0f5922915772415710985dd33
ck <1052650769@qq.com>
2017-07-03 09:13:11 +0800

    修改

--- index/user.php
+++ index/user.php
@@ -1,8 +1,17 @@
 <?php
+/*
+ 本软件版权归作者所有,在投入使用之前注意获取许可
+ 作者：北京市普艾斯科技有限公司
+ 项目：simcms_锐车1.0
+ 电话：010-58480317
+ Q  Q: 228971357
+ 网址：http://www.simcms.net
+ simcms.net保留全部权力，受相关法律和国际公约保护，请勿非法修改、转载、散播，或用于其他赢利行为，并请勿删除版权声明。
+*/
 if (!defined('APP_IN')) exit('Access Denied');
 
-include ('page.php');echo INC_DIR . 'html.func.php';
-include(INC_DIR . 'html.func.php');echo 555;
+include ('page.php');
+//include(INC_DIR . 'html.func.php');
 // 验证邮箱地址
 if (!empty($_POST['param']) and $_POST['name'] == "email") {
 	$data = $db -> row_count('member', "email='" . $_POST['param'] . "' and id!={$_SESSION['USER_ID']}");
@@ -75,7 +84,7 @@ $ac = isset($_REQUEST['a']) && isset($ac_arr[$_REQUEST['a']]) ? $_REQUEST['a'] :
 
 $tpl -> assign('ac_arr', $ac_arr);
 $tpl -> assign('ac', $ac);
-echo 333444;
+
 // 修改密码处理ajax后台处理
 if (!empty($_GET['ajax']) && isset($_GET['oldpassword'])) {
 	if ($userinfo['password'] == md5($_GET['oldpassword'])) {
@@ -150,7 +159,7 @@ if (!empty($_GET['ajax']) && isset($_GET['rentid'])) {
 if ($ac == 'index') {
 	$userinfo['last_login_time'] = date("Y-m-d H:i:s", $userinfo['last_login_time']);
 	$tpl -> assign('user', $userinfo); 
-echo 333;
+
 	$tpl -> display('default/' . $settings['templates'] . '/user.html');
 	exit;
 } 
