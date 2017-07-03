c395ad90099e069286a033538a54a66a3b78cb5e
ck <1052650769@qq.com>
2017-07-03 13:23:15 +0800

    修改

--- m/index/upload_car.php
+++ m/index/upload_car.php
@@ -62,7 +62,7 @@ if (empty($_FILES) === false) {
 	$file_size = $_FILES['imgFile']['size'];
 	//检查文件名
 	if (!$file_name) {
-		alert("请选择文件。");
+		alert("请选择11文件。");
 	}
 	//检查目录
 	if (@is_dir($save_path) === false) {
