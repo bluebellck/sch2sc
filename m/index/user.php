f82dc039b5c151ca3201882e9c3a8d05bccdf9af
ck <1052650769@qq.com>
2017-07-04 14:20:38 +0800

    修改

--- m/index/user.php
+++ m/index/user.php
@@ -536,7 +536,7 @@ elseif ($ac == 'addpicture') {
 	else {
 		$configure_list = array();
 		$data = array('p_mainpic'=>'','p_backpic'=>'','p_foresightpic'=>'','p_leftforepic'=>'','p_rightforepic'=>'','p_backsightpic'=>'','p_leftbackpic'=>'','p_rightbackpic'=>'','p_leftpic'=>'','p_sidepic'=>'','p_wheelpic'=>'','p_rightpic'=>'','p_driverlicpic'=>'','p_drivinglicpic'=>'');
-		if (!empty($carid)){echo $carid
+		if (!empty($carid)){echo $carid;             
 			$r = $db -> row_select_one('cars', "p_id=".$carid);var_dump($r);
 			$arr = explode('|',$r['p_pics']);
 			$count = count($arr);
