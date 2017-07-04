cf6795edbed547bc5973886e036380067ea5bc4a
ck <1052650769@qq.com>
2017-07-04 10:43:05 +0800

    修改

--- m/index/user.php
+++ m/index/user.php
@@ -250,8 +250,24 @@ elseif ($ac == 'carlist') {
 		$keywords = $_GET['keywords'];
 		$where .= " and (name like '%{$keywords}%' or mobilephone like '%{$keywords}%')";
 	}
-	echo 111;
 	include('../'.INC_DIR . 'Page.class.php');
+	$Page = new Page($db -> tb_prefix . 'cars', $where, '*', '50', 'issell asc,listtime desc');
+	$list = $Page -> get_data();echo 888;
+	foreach($list as $key => $value) {
+		$list[$key]['listtime'] = date('Y-m-d H:i:s', $value['listtime']);
+		$list[$key]['p_addtime'] = date('Y-m-d H:i:s', $value['p_addtime']);
+		if (!empty($value['p_model'])) $list[$key]['p_modelname'] = $array_model[$value['p_model']];
+		$list[$key]['p_url'] = HTML_DIR . "buycars/" . date('Y/m/d', $value['p_addtime']) . "/" . $value['p_id'] . ".html";
+	} 
+	echo 123;
+	$button_basic = $Page -> button_basic();
+	$button_select = $Page -> button_select();
+	$pageid = $Page -> page;
+	$tpl -> assign('button_basic', $button_basic);
+	$tpl -> assign('button_select', $button_select);
+	$tpl -> assign('carslist', $list);
+	$tpl -> assign('currpage', $pageid);
+	$tpl -> display('default/m/user_carlist.html');
 	exit;
 } 
 // 添加或修改车源
