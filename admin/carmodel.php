c122ecc8424060526f1ec9d119f2d22c782121e0
ck <1052650769@qq.com>
2017-07-07 14:14:38 +0800

    修改

--- admin/carmodel.php
+++ admin/carmodel.php
@@ -1,4 +1,5 @@
 <?php
+
 if (!defined('APP_IN')) exit('Access Denied');
 
 // 品牌选择
@@ -33,49 +34,29 @@ $tpl->assign( 'ac', $ac );
 if ($ac == 'list')
 {
 	if(!empty($_GET['p_subbrand'])){
-	   $where='b_id='.$_GET['p_subbrand'];
-	   $list = $db -> row_select('brand', $where,'b_id,b_name,b_parent');
-	   $list01=$db -> row_select_one('brand', 'b_id='.$list[0]['b_parent'],'b_id,b_parent');
+	   $where='series_id='.$_GET['p_subbrand'];
+	   $list = $db -> row_select('styles', $where);
 	   foreach($list as $key => $value){
-			$subbrandlist = $db->row_select('brand','b_parent = '.$value['b_id']);
-			foreach($subbrandlist as $subkey => $subvalue){
-				$subsubbrandlist = $db->row_select('brand','b_parent = '.$subvalue['b_id']);
-				$subbrandlist[$subkey]['subbrands_list']=$subsubbrandlist;
-			}
-			$list[$key]['brands_list']=$subbrandlist;
+		   	 $subbrandlist=$db ->row_select_one('series','series_id='.$value['series_id']);
+			 $list[$key]['brandname']=$subbrandlist['series_groupname'];
+			$list[$key]['seriesname']=$subbrandlist['series_name'];			
 		}
 		$tpl->assign( 'types', 1 );
-		//$tpl -> assign('select_brand', $select_brand);
-		$select_brand = select_make($list01['b_parent'], $commoncache['markbrandlist'], '请选择品牌');
-		$select_subbrand = select_subbrand(intval($list[0]['b_id']));
+		$select_brand = select_make($list[0]['brand_id'], $commoncache['markbrandlist'], '请选择品牌');
+		$select_subbrand = select_subbrand(intval($_GET['p_subbrand']));
 		$tpl -> assign('select_brand', $select_brand);
 		$tpl -> assign('select_subbrand', $select_subbrand);
 	}
 	else{
-		$where = 'classid = 5';
+		$where = '1=1';
 		include(INC_DIR.'Page.class.php');
-		$Page = new Page($db->tb_prefix.'brand',$where,'*',30,'b_id');
+		$Page = new Page($db->tb_prefix.'styles',$where,'*',30,'styles_id');
 		$list = $Page->get_data();
 		$page = $Page -> page;
 		foreach($list as $key => $value){
-			if(!empty($value['b_parent'])){
-				$data01 = $db -> row_select_one('brand','b_id='.$value['b_parent'],'b_parent,b_name,b_id');
-				$list[$key]['styles']=$data01['b_name'];
-				$list[$key]['f_id']=$data01['b_id'];
-				if(!empty($data01)){
-					$data02 = $db -> row_select_one('brand','b_id='.$data01['b_parent'],'b_parent,b_name,b_id');
-					$list[$key]['model']=$data02['b_name'];
-				}
-				if(!empty($data02)){
-					$data03 = $db -> row_select_one('brand','b_id='.$data02['b_parent'],'b_parent,b_name,b_id');
-					$list[$key]['brand02']=$data02['b_name'];
-				}
-				if(!empty($data03)){
-					$data04 = $db -> row_select_one('brand','b_id='.$data03['b_parent'],'b_parent,b_name,b_id');
-					$list[$key]['brand01']=$data04['b_name'];
-				}
-			}
-			
+			$sublist=$db->row_select_one('series','series_id='.$value['series_id']);
+			$list[$key]['brandname']=$sublist['series_groupname'];			
+			$list[$key]['seriesname']=$sublist['series_name'];			
 		}
 		$tpl->assign( 'types', 2 );
 		$button_basic = $Page->button_basic();
@@ -92,13 +73,14 @@ if ($ac == 'list')
     $tpl->assign( 'match_arr',	$match_arr );
     $tpl->assign( 'brandlist', $list );
     $tpl->display( 'admin/carmodel_list.html' );
+	print_r($brandlist);
     exit;
 }
 //单条删除
 elseif ($ac == 'del')
 {
-    $s_id = isset($_GET['b_id']) ? intval($_GET['b_id']) : showmsg('缺少ID',-1);
-    $rs = $db->row_delete('brand',"b_id=$b_id");
+    $s_id = isset($_GET['styles_id']) ? intval($_GET['styles_id']) : showmsg('缺少ID',-1);
+    $rs = $db->row_delete('styles',"styles_id=$styles_id");
 }
 //批量删除
 elseif ($ac == 'bulkdel')
@@ -113,7 +95,7 @@ elseif ($ac == 'bulksort')
     if (empty($_POST['bulkid'])) showmsg('没有选中任何项',-1);
     foreach ($_POST['bulkid'] as $k => $v)
     {
-        $rs = $db->row_update('brand',array('orderid'=>$_POST['orderid'][$v]),"b_id=".intval($v));
+        $rs = $db->row_update('styles',array('orderid'=>$_POST['orderid'][$v]),"b_id=".intval($v));
     }
 }
 //添加
@@ -122,93 +104,22 @@ elseif ($ac == 'add' || $ac == 'edit')
     //添加或修改
     if (submitcheck('a'))
     {	
-      //  $arr_not_empty = array('b_name4'=>'款式名称不可为空');
-        //can_not_be_empty($arr_not_empty,$_POST);
+          $arr_not_empty = array('styles_year'=>'款式名称不可为空','styles_name'=>'车型名称不可为空');
+          can_not_be_empty($arr_not_empty,$_POST);
         if ($ac == 'add')
         {
-			if($_POST['isaddmodel']==1){
-			//四级品牌
-			$post=post('b_name','b_parent','mark','classid');
-            $listthree = $db->row_select_one('brand','b_id='.intval($_POST['p_subbrand']));
-			$post['b_parent']=$listthree['b_id'];
-			$post['mark']=$listthree['mark'];
-			$post['b_name']=$_POST['b_name4'];
-			$post['classid']=4;
-			$rs = $db->row_insert('brand',$post);
-			$insertid = $db -> insert_id();
-		  // 五级品牌
-		   if(!empty($_POST['b_name5'])){
-		    $post['b_name']=$_POST['b_name5'];
-			$post['classid']=5;
-			$post['b_parent']=$insertid;
-			$post['mark']=$listthree['mark'];
-            $rs = $db->row_insert('brand',$post);
-		   }
-			}else{
-		  // 五级品牌
-		  if(!empty($_POST['b_name5'])){
-            $listfour = $db->row_select_one('brand','b_id='.intval($_POST['p_subsubbrand']));
-		    $post['b_name']=$_POST['b_name5'];
-			$post['classid']=5;
-			$post['b_parent']=$listfour['b_id'];
-			$post['mark']=$listfour['mark'];
-            $rs = $db->row_insert('brand',$post);
-		  }
-			}
+			$post=post('brand_id','series_id','styles_year','styles_name');
+			$post['brand_id']=$_POST['p_brand'];
+			$post['series_id']=$_POST['p_subbrand'];
+			$rs = $db->row_insert('styles',$post);
         }
-        else
-		{ 	
-			if($_POST['isaddmodel']==1){
-			//四级品牌
-			$post=post('b_name','b_parent','mark','classid');
-            $listthree = $db->row_select_one('brand','b_id='.intval($_POST['p_subbrand']));
-			$post['b_parent']=$listthree['b_id'];
-			$post['mark']=$listthree['mark'];
-			$post['b_name']=$_POST['b_name4'];
-			$post['classid']=4;
-			$rs = $db->row_insert('brand',$post);
-			$insertid = $db -> insert_id();
-		  // 五级品牌
-		   // $listfour = $db->row_select_one('brand','b_parent='.intval($_POST['p_subbrand']).' and b_name='.'"'.$_POST['b_name4'].'"');
-		    $post['b_name']=$_POST['b_name5'];
-			$post['classid']=5;
-			$post['b_parent']=$insertid;
-			$post['mark']=$listthree['mark'];
-            $rs = $db->row_update('brand',$post,'b_id='.$_POST['b_id']);
-			}else{
-		  // 五级品牌
-            $listfour = $db->row_select_one('brand','b_id='.intval($_POST['p_subsubbrand']));
-		    $post['b_name']=$_POST['b_name5'];
-			$post['classid']=5;
-			$post['b_parent']=$listfour['b_id'];
-			$post['mark']=$listfour['mark'];
-            $rs = $db->row_update('brand',$post,'b_id='.$_POST['b_id']);
-			}
-		}
     }
     //转向添加或修改页面
     else 
     {
-		    if (empty($_GET['b_id'])) {
-		    $data = array('b_id'=>'','b_parent'=>'','b_name'=>'','mark'=>'');
 		    $select_brand = select_make(0, $commoncache['markbrandlist'], '请选择品牌');
-		    $select_subbrand = select_subbrand(0);
-		    $select_fourbrand ='';
-		}
-            else {
-	        $data5 = $db->row_select_one('brand',"b_id=".intval($_GET['b_id']));
-		    $data4 = $db->row_select_one('brand',"b_id=".$data5['b_parent']);
-		    $data3 = $db->row_select_one('brand',"b_id=".$data4['b_parent']);
-		    $data2 = $db->row_select_one('brand',"b_id=".$data3['b_parent']);
-		    $select_brand = select_make($data2['b_parent'], $commoncache['markbrandlist'], '请选择品牌');
-		    $select_subbrand = select_subbrand($data3['b_id']);
-		    $select_fourbrand = select_make($data4['b_id'], arr_brand($data3['b_id']));
-		    $tpl -> assign('subsubsubsublist', $data5);
-		}
 		    $tpl->assign( 'ac', $ac );
 		    $tpl -> assign('select_brand', $select_brand);
-		    $tpl -> assign('select_subbrand', $select_subbrand);
-		    $tpl -> assign('select_fourbrand', $select_fourbrand);
             $tpl->display( 'admin/add_carmodel.html' );
             exit;	
     }
@@ -218,17 +129,17 @@ elseif ($ac == 'editcarstyle')
 {
     if (submitcheck('a'))
     {	
-        $arr_not_empty = array('b_name4'=>'款式名称不可为空');
+        $arr_not_empty = array('styles_year'=>'款式名称不可为空','styles_name'=>'车型名称不可为空');
         can_not_be_empty($arr_not_empty,$_POST);
-		//四级品牌
-		$post=post('b_name');
-		$post['b_name']=$_POST['b_name4'];
-		$rs = $db->row_update('brand',$post,'b_id='.$_POST['b_id']);	        
+		$post=post('styles_year','styles_name');
+		$post['styles_year']=$_POST['styles_year'];
+		$post['styles_name']=$_POST['styles_name'];
+		$rs = $db->row_update('styles',$post,'styles_id='.$_POST['styles_id']);	        
     }
     //转向修改页面
     else 
     {
-            $data4 = $db->row_select_one('brand',"b_id=".$_GET['b_id']);
+            $data4 = $db->row_select_one('styles',"styles_id=".$_GET['styles_id']);
 			$tpl->assign( 'subsubsublist', $data4 );
 		    $tpl->assign( 'ac', $ac );
             $tpl->display( 'admin/edit_carstyle.html' );
