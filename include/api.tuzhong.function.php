5b51d41a9c0fe52abe5bc59abdd0f8d110baf4cb
ck <1052650769@qq.com>
2017-07-10 19:22:37 +0800

    修改

--- include/api.tuzhong.function.php
+++ include/api.tuzhong.function.php
@@ -3,6 +3,7 @@ function api_tuzhong_all($data,$uid,$type){
 	$model_arr = array('1' => 1,'3' => 1,'4' => 1,'5' => 1,'6' => 1,'7' => 4,'8' => 3,'9' => 2,'38' => 5,'41' => 1);
 	$pics_arr = explode('|',$data['p_pics']);
 	$arr = array();
+	$arr['type'] = $type;
 	$arr['uid'] = $uid;
 	$arr['cheyuan_id'] = $data['p_id'];
 	$arr['p_brand'] = $data['p_brand'];
