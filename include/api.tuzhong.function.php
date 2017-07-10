55941baf86f6de6fbc1877a1bc2017d2e060d88f
ck <1052650769@qq.com>
2017-07-10 20:42:56 +0800

    修改

--- include/api.tuzhong.function.php
+++ include/api.tuzhong.function.php
@@ -1,4 +1,5 @@
 <?php
+echo 2222;
 function api_tuzhong_all($data,$uid,$type){
 	$model_arr = array('1' => 1,'3' => 1,'4' => 1,'5' => 1,'6' => 1,'7' => 4,'8' => 3,'9' => 2,'38' => 5,'41' => 1);
 	$pics_arr = explode('|',$data['p_pics']);
@@ -62,6 +63,6 @@ function api_tuzhong_all($data,$uid,$type){
 
 }
 
-
+echo 333;
 
 ?>
